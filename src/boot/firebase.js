a271e105dc3106c41e943cdf27d59823f002cc7a
george <george88ch@gmail.com>
2021-01-22 17:53:03 +0100

    Firebase installed

---
+++ src/boot/firebase.js
@@ -0,0 +1,35 @@
+import { firebase } from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+// firebase init - add your own config here
+const firebaseConfig = {
+  apiKey: "AIzaSyDs4vGBWxft5--Lj00RlWtrqM7xp-6eTZ4",
+  authDomain: "scontrol-d4694.firebaseapp.com",
+  projectId: "scontrol-d4694",
+  storageBucket: "scontrol-d4694.appspot.com",
+  messagingSenderId: "651617581016",
+  appId: "1:651617581016:web:cec63aeca2f3af4d6c24ad",
+  measurementId: "G-K8BX8CGMYN"
+};
+firebase.initializeApp(firebaseConfig);
+
+// utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// collection references
+const usersCollection = db.collection("users");
+const postsCollection = db.collection("posts");
+const commentsCollection = db.collection("comments");
+const likesCollection = db.collection("likes");
+
+// export utils/refs
+export {
+  db,
+  auth,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection
+};
