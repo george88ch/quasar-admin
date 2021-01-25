98fb7b77fc3e1a43706d238fe7ad4463103ab384
george <george88ch@gmail.com>
2021-01-25 08:19:23 +0100

    test2

--- src/components/cards/CardPanel.vue
+++ src/components/cards/CardPanel.vue
@@ -162,15 +162,7 @@ export default {
           avatar: "https://avatars2.githubusercontent.com/u/34883558?s=400&v=4",
           time: "10:42 PM",
         },
-        {
-          id: 6,
-          name: "Winfield Stapforth",
-          msg:
-            " -- I'll be in your neighborhood doing errands this\n" +
-            "            weekend. Do you want to grab brunch?",
-          avatar: "https://cdn.quasar.dev/img/avatar6.jpg",
-          time: "11:17 AM",
-        },
+
         {
           id: 1,
           name: "Boy",
